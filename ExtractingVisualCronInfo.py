f9fdc76b8d75cf903bd72e8f95b2df9ed8e656b7
Wayne Davey <waynedavey89@hotmail.com>
2022-09-30 08:36:28 +0100

    Access API and reading required JSON information using local instance of VisualCron currently

--- ExtractingVisualCronInfo.py
+++ ExtractingVisualCronInfo.py
@@ -1,29 +1,40 @@
+import os
 import requests
-from requests.api import request
+from requests import request
 from requests.models import Response
 
+
 def main():
-    machineName = "WAYNE-PC"
-    api_url = "http://localhost:8001/VisualCron/json/logon?username=Wayne&password=qiagen09&expire=360000"
-    response = requests.get(api_url)
-    dict = {}
-    dict = response.json()
-    authToken = dict["Token"]
-    getJobInfo(authToken)
+    machineName = os.environ['COMPUTERNAME']
+    connectAPI(machineName)
+
+def connectAPI(machineName):
+    apiUrl = "http://localhost:8001/VisualCron/json/logon?username=wayne&password=wayne&expire=360000"
+    apiResponse = requests.get(apiUrl)
+    jsonResponse = {}
+    jsonResponse = apiResponse.json()
+    authToken = jsonResponse['Token']
+    print("Token Acquired: {}\n".format(authToken))
+    getJobInfo(machineName, authToken)
     
+def getJobInfo(machineName, authToken):
+    connectUrl = "http://localhost:8001/VisualCron/json/Job/List?token={}".format(authToken)
+    jobListResponse = requests.get(connectUrl)
+    jsonResult = []
+    jsonResult = jobListResponse.json()
+    listLength = len(jsonResult)
+    print("Machine Name: {}".format(machineName))
+    i = 0
+    while i < listLength:
+        jobName = jsonResult[i]['Name']
+        jobDesc = jsonResult[i]['Description']
+        groupName = jsonResult[i]['Group']
+        jobId = jsonResult[i]['Id']
+        print("Job Name: {}".format(jobName))
+        print("Job Description: {}".format(jobDesc))
+        print("Group Name: {}".format(groupName))
+        print("Job Id: {}\n".format(jobId))
+        i += 1
     
-
-
-def getJobInfo(authToken):
-    connect_url = "http://localhost:8001/VisualCron/json/Job/List?token={}".format(authToken)
-    response2 = requests.get(connect_url)
-    result = []
-    result = response2.json()
-    JobName = result[0]['Name']
-    JobDescription = result[0]['Description']
-    print(JobName + " - " + JobDescription)
-   
-
-
 if __name__ == "__main__":
     main()
