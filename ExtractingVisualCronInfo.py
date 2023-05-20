5f8288f8e19eb36adf93abaa982e9857fa4c4237
Wayne Davey <waynedavey89@hotmail.com>
2023-05-20 09:55:51 +0100

    Added comments before each method

--- ExtractingVisualCronInfo.py
+++ ExtractingVisualCronInfo.py
@@ -3,6 +3,7 @@ from requests import request
 from requests.models import Response
 import datetime
 
+# Main method. Read in the appsettings.json values for Connection String, username, password and timeout.
 def main():
     ctypes.windll.kernel32.SetConsoleTitleW('Extracting VisualCron Information')
     print("VisualCron Data Retrieval starting...")
@@ -24,26 +25,25 @@ def main():
         for machineName in machines:
             apiUrl = "http://{}:8001/VisualCron/json/logon?username={}&password={}&expire={}".format(machineName, username, password, timeout)
             authToken = connectAPI(machineName, apiUrl)
-            print("Token Acquired: {} on Server Name: {}".format(authToken, machineName))
             print("[{}] Token Acquired: {} on Server Name: {}".format(datetime.datetime.now().strftime("%H:%M:%S"),authToken, machineName))
             getJobInfo(machineName, authToken, conn)
     except FileNotFoundError:
-        print("No appsettings.json file can be found.")
         print("[{}] No appsettings.json file can be found.".format(datetime.datetime.now().strftime("%H:%M:%S")))
         quit()
     conn.close()
     
+
+# Check the connection string to ensure it connects without issue, if it fails, exit the program.
 def getConnectionString(connectionString):
     try:
         conn = pypyodbc.connect(connectionString)
-        print("Connected to SQL Server...")
         print("[{}] Connected to SQL Server...".format(datetime.datetime.now().strftime("%H:%M:%S")))
         return conn
     except pypyodbc.Error:
-        print("Failed to connect to SQL Server")
         print("[{}] Failed to connect to SQL Server".format(datetime.datetime.now().strftime("%H:%M:%S")))
         quit()
 
+# Retrieve the list of Machine Names from the table 'visualcron.MachineNames'. Then remove the ' from either side of the machine name from the returned result.
 def getMachineNames(conn):
     machines = []
     cursor = conn.cursor()
@@ -56,6 +56,7 @@ def getMachineNames(conn):
             machines.append(line_out)        
     return machines
 
+# Connect to the WebAPI for each machine, to ensure that the API is enabled and can be accessed in order to retrieve a token.
 def connectAPI(machineName, apiUrl):
     try:
         apiResponse = requests.get(apiUrl)
@@ -64,16 +65,16 @@ def connectAPI(machineName, apiUrl):
         authToken = jsonResponse['Token']
         return authToken
     except requests.ConnectionError:
-        print("Unable to connect to URL, please check this is a valid URL, or ensure the WebAPI option is enabled in VisualCron on target machine {}\n".format(machineName))  
         print("[{}] Unable to connect to URL, please check this is a valid URL, or ensure the WebAPI option is enabled in VisualCron on target machine {}\n".format(datetime.datetime.now().strftime("%H:%M:%S"), machineName))
     
+
+# Retrieve the job information for the specific fields from the WebAPI of each machine, and call the Stored Procedure to insert into the database table 'visualcron.VisualCronData'
 def getJobInfo(machineName, authToken, conn):
     connectUrl = "http://{}:8001/VisualCron/json/Job/List?token={}".format(machineName, authToken)
     jobListResponse = requests.get(connectUrl)
     jsonResult = []
     jsonResult = jobListResponse.json()
     listLength = len(jsonResult)
-    print("Machine Name: {}".format(machineName))
     print("[{}] Machine Name: {}".format(datetime.datetime.now().strftime("%H:%M:%S"), machineName))
     cursor = conn.cursor()
     i = 0
@@ -93,14 +94,6 @@ def getJobInfo(machineName, authToken, conn):
             dateLastExecution = lastExecution.replace('+00:00', '')
             tempDateLastExecution = dateLastExecution.replace('T', ' ')
             dateLastExecution = tempDateLastExecution
-            print("Job Name: {}".format(jobName))
-            print("Job Description: {}".format(jobDesc))
-            print("Group Name: {}".format(groupName))
-            print("Job Id: {}".format(jobId))
-            print("Job Status: {}".format(jobStatus))
-            print("Last Execution: {}".format(lastExecution))
-            print("Number of Executes: {}".format(numberOfExecutes))
-            print("Date of Last Execution: {}".format(dateLastExecution))
             print("[{}] Job Name: {}".format(datetime.datetime.now().strftime("%H:%M:%S"), jobName))
             print("[{}] Job Description: {}".format(datetime.datetime.now().strftime("%H:%M:%S"), jobDesc))
             print("[{}] Group Name: {}".format(datetime.datetime.now().strftime("%H:%M:%S"), groupName))
@@ -110,20 +103,16 @@ def getJobInfo(machineName, authToken, conn):
             print("[{}] Number of Executes: {}".format(datetime.datetime.now().strftime("%H:%M:%S"), numberOfExecutes))
             print("[{}] Date of Last Execution: {}".format(datetime.datetime.now().strftime("%H:%M:%S"), dateLastExecution))
         except TypeError:
-            print("Json Result list is likely empty, hence cannot iterate through an empty list")
             print("[{}] Json Result list is likely empty, hence cannot iterate through an empty list".format(datetime.datetime.now().strftime("%H:%M:%S")))
         if len(jobName) > 0:
             try:
                 paramaters = (machineName, jobId, jobName, jobDesc or None, groupName, jobStatus, dateLastExecution, numberOfExecutes)
                 cursor.execute("{CALL visualcron.AddVisualCronInfo (?,?,?,?,?,?,?,?)}", paramaters)
-                print("Executing Stored Procedure: visualcron.AddVisualCronInfo \n")
                 print("[{}] Executing Stored Procedure: visualcron.AddVisualCronInfo \n".format(datetime.datetime.now().strftime("%H:%M:%S")))
                 conn.commit()
             except NameError:
-                print("Cannot execute Stored Procedure")
                 print("[{}] Cannot execute Stored Procedure".format(datetime.datetime.now().strftime("%H:%M:%S")))
         else:
-            print("Job name is blank, no data to insert")
             print("[{}] Job name is blank, no data to insert".format(datetime.datetime.now().strftime("%H:%M:%S")))
         i += 1
    
